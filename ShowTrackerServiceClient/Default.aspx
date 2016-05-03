58e6fc9c65948d91f4a7542e56e7b3eb9aa05c46
Tedwinder <TedCallow76@gmail.com>
2016-05-03 12:15:32 -0700

    web page

---
+++ ShowTrackerServiceClient/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+
+        <asp:DropDownList ID="Venue" runat="server"></asp:DropDownList>
+        <asp:DropDownList ID="Artist" runat="server"></asp:DropDownList>
+        <asp:GridView ID="ShowInVenue" runat="server"></asp:GridView>
+        <asp:GridView ID="ShowInArtist" runat="server"></asp:GridView>
+        <asp:Button ID="GetShowsInVenue" runat="server" Text="GetShowsInVenue" OnClick="GetShowInfo_Click" />
+        <asp:Button ID="GetShowsInArtist" runat="server" Text="GetShowsInArtist" OnClick="GetShowInfo_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ ShowTrackerServiceClient/Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    ShowTrackerServiceReference.ServiceClient stsc
+       = new ShowTrackerServiceReference.ServiceClient();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+            FillVenueDropDown();
+        FillArtistDropDown();
+    }
+
+
+    protected void FillArtistDropDown()
+    {
+
+        string[] artists = stsc.GetArtist();
+        Artist.DataSource = artists;
+        Artist.DataBind();
+
+    }
+
+    protected void FillVenueDropDown()
+    {
+
+        string[] venues = stsc.GetVenue();
+        Venue.DataSource = venues;
+        Venue.DataBind();
+
+    }
+
+    protected void GetShowInfo_Click(object sender, EventArgs e)
+    {
+        
+
+        string ArtistChoice = Artist.SelectedItem.Text;
+
+
+        ShowTrackerServiceReference.ShowsInArtist[] shows = stsc.GetShowsInArtist(ArtistChoice);
+        ShowInArtist.DataSource = shows;
+        ShowInArtist.DataBind();
+
+
+
+        string VenueChoice = Venue.SelectedItem.Text;
+
+
+        ShowTrackerServiceReference.ShowsInVenue[] vens = stsc.GetShowsInVenue(VenueChoice);
+        ShowInArtist.DataSource = vens;
+        ShowInArtist.DataBind();
+    }
+}
